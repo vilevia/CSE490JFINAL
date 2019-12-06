4e253e1893848f1935639cc02e96ec20c787905e
TStrong85 <33169441+TStrong85@users.noreply.github.com>
2019-12-05 17:10:44 -0800

    Imported ShootingGallery prototype

---
+++ fInAl/Assets/_ShootingGallery/ClickToThrow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Add this component to a camera to allow projectiles to be thrown with on-screen mouse clicks.
+/// </summary>
+public class ClickToThrow : MonoBehaviour
+{
+    public float throwSpeed = 5f;
+    public float lifetime = 10f;
+    public Projectile projectile;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start() {
+        // Sets up the camera automatically
+        if (cam == null) {
+            cam = GetComponent<Camera>();
+            if (cam == null) {
+                cam = Camera.main;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Throw when left mouse button is clicked
+        if (Input.GetMouseButtonDown(0)) {
+            LaunchObject();
+        }
+    }
+
+    void LaunchObject() {
+        Ray aimRay = cam.ScreenPointToRay(Input.mousePosition);
+        if (projectile == null) {
+            Debug.LogWarning("Can't launch object: the projectile prefab is null");
+        }
+        Projectile instObj = (Projectile)Instantiate(projectile, aimRay.GetPoint(cam.nearClipPlane), cam.transform.rotation);
+        instObj.rb.velocity = aimRay.direction * throwSpeed;
+        Destroy(instObj.gameObject, lifetime);
+    }
+}
---
+++ fInAl/Assets/_ShootingGallery/ProjectileTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+/// <summary>
+/// This is a target for use in the shooting gallery
+/// If hit by an object with a Projectile component, runs the OnProjectileHit event show in the inspector 
+/// </summary>
+public class ProjectileTarget : MonoBehaviour
+{
+    public UnityEvent OnProjectileHitEvent;
+
+    public void HitReaction(Projectile projectile){
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Projectile projectile = collision.gameObject.GetComponent<Projectile>();
+        // A projectile hit this target! React appropriately
+        if (projectile != null) {
+            // Do general reaction using event in inspector
+            Debug.Log("ProjectileHitEvent sent: " + projectile.name);
+            if (OnProjectileHitEvent != null) {
+                OnProjectileHitEvent.Invoke();
+            }
+            // Do reaction defined in this class
+            HitReaction(projectile);
+        }
+    }
+}
