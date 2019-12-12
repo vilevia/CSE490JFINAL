313e6e98a56473185085eef5fa2c11e636f9247f
Terrell Strong <stront2@uw.edu>
2019-12-12 13:51:58 -0800

    VR controller and Corruption added to museum

--- fInAl/Assets/Unit Tests/FasterThrowing/ThrowableObject.cs
+++ fInAl/Assets/Unit Tests/FasterThrowing/ThrowableObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowableObject : OVRGrabbable
-{  
+{
     public float throwSpeedMultiplier = 5;
     // Maps from the speed of the hand when this is released to how much of the speed multiplier should be applied (values should range from 0 to 1)
     public AnimationCurve releaseVelocityThresholdCurve;
