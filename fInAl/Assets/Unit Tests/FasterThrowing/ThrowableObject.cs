1ea7a9ccb76d1736ede3b9a372488da444edd603
Terrell Strong <stront2@uw.edu>
2019-12-12 15:24:02 -0800

    Grab Events added to ThrowableObject

--- fInAl/Assets/Unit Tests/FasterThrowing/ThrowableObject.cs
+++ fInAl/Assets/Unit Tests/FasterThrowing/ThrowableObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ThrowableObject : OVRGrabbable
 {
@@ -8,15 +9,31 @@ public class ThrowableObject : OVRGrabbable
     // Maps from the speed of the hand when this is released to how much of the speed multiplier should be applied (values should range from 0 to 1)
     public AnimationCurve releaseVelocityThresholdCurve;
 
+    public UnityEvent OnGrabBeginEvent;
+    public UnityEvent OnGrabEndEvent;
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint) {
+        base.GrabBegin(hand, grabPoint);
+        if(OnGrabBeginEvent != null) {
+            OnGrabBeginEvent.Invoke();
+        }
+
+    }
+
     // Start is called before the first frame update
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity) {
         base.GrabEnd(linearVelocity, angularVelocity);
-        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        if (OnGrabEndEvent != null) {
+            OnGrabEndEvent.Invoke();
+        }
 
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
         float velocityMultiplier = throwSpeedMultiplier * releaseVelocityThresholdCurve.Evaluate(linearVelocity.magnitude);
         velocityMultiplier = Mathf.Max(1, velocityMultiplier);
 
         rb.velocity *= velocityMultiplier;
         Debug.Log(linearVelocity.magnitude);
+
+
     }
 }
